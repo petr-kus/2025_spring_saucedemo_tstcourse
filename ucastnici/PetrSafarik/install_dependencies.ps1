af8b0b20a2090dfc0995770793c65b4cf3287d61
Petr Safarik <peta.safa@centrum.cz>
2025-03-14 14:29:46 +0100

    install_dependencies.ps1 - pip freeze removed

--- ucastnici/PetrSafarik/install_dependencies.ps1
+++ ucastnici/PetrSafarik/install_dependencies.ps1
@@ -4,4 +4,3 @@ Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -Scope Process
 Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -Scope CurrentUser
 python.exe -m pip install --upgrade pip
 pip install -r requirements.txt
-pip freeze > requirements.txt
