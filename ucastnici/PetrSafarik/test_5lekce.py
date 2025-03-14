a0ba8a0eb3306bc66309168a5d8a710afbf3e3de
Petr Safarik <peta.safa@centrum.cz>
2025-03-14 14:25:05 +0100

    py_script_v2

--- ucastnici/PetrSafarik/test_5lekce.py
+++ ucastnici/PetrSafarik/test_5lekce.py
@@ -8,23 +8,52 @@ import time
 driver = webdriver.Firefox()
 driver.get("https://www.saucedemo.com/")
 
-#time.sleep(1)  # Wait for page to load
-
-# Find username and password fields and login button
+# Find and fill username and password fields and click on login button
 field_username = driver.find_element(By.ID, "user-name")
 field_username.send_keys("standard_user")
 
 field_password = driver.find_element(By.ID, "password")
 field_password.send_keys("secret_sauce")
 
-button_submit = driver.find_element(By.ID, "login-button")
-button_submit.click()
+button_login = driver.find_element(By.ID, "login-button")
+button_login.click()
+
+# Add item Backpack to the cart
 
 backpack_to_cart = driver.find_element(By.ID, "add-to-cart-sauce-labs-backpack")
 backpack_to_cart.click()
 
-button_cart = driver.find_element(By.CLASS_NAME, "shopping_cart_link")
+# Enter the cart
+
+#button_cart = driver.find_element(By.CLASS_NAME, "shopping_cart_link")
+button_cart = driver.find_element(By.XPATH, "//a[@data-test='shopping-cart-link']")
 button_cart.click()
+
+# Enter checkout page
+
+button_checkout = driver.find_element(By.ID, "checkout")
+button_checkout.click()
+
+# Find and fill First Name, Last Name and Postal code
+
+field_first_name = driver.find_element(By.ID, "first-name")
+field_first_name.send_keys("Petr")
+
+field_second_name = driver.find_element(By.ID, "last-name")
+field_second_name.send_keys("Safarik")
+
+field_postal_code = driver.find_element(By.ID, "postal-code")
+field_postal_code.send_keys("109 00")
+
+# click on Continute button
+
+button_login = driver.find_element(By.ID, "continue")
+button_login.click()
+
+time.sleep(5)  # Wait to see checkout overview
+
+button_finish = driver.find_element(By.ID, "finish")
+button_finish.click()
 
 time.sleep(5)  # Wait to see result
 
